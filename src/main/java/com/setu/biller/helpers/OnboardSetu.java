612213b59a243edfa3908c5e4e7058e48c7a198a
Debashis <debashis@Deepanshus-MacBook-Pro.local>
2020-05-27 23:43:52 +0530

    converted onboard.json to string due to resource exclude issue

--- src/main/java/com/setu/biller/helpers/OnboardSetu.java
+++ src/main/java/com/setu/biller/helpers/OnboardSetu.java
@@ -1,33 +1,21 @@
 package com.setu.biller.helpers;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.util.ResourceUtils;
 
 @Configuration
 public class OnboardSetu {
+
+    private String onboard = "{\"customerParameters\":[{\"attributeName\":\"mobileNumber\",\"displayName\":\"Mobile number\",\"dataType\":\"string\",\"minLength\":10,\"maxLength\":10,\"isMandatory\":true},{\"attributeName\":\"phoneNumber\",\"displayName\":\"Phone number\",\"dataType\":\"string\",\"minLength\":10,\"maxLength\":10,\"isMandatory\":false},{\"attributeName\":\"mail\",\"displayName\":\"Email Id\",\"dataType\":\"string\",\"minLength\":5,\"maxLength\":50,\"isMandatory\":false}]}";
 
     @Bean
     @Scope(value = "singleton")
     public JsonArray getCustomerParameter(){
         JsonParser parser = new JsonParser();
-        try{
-            File onboard = ResourceUtils.getFile("classpath:onboard.json");
-            JsonArray parameters = parser.parse(new FileReader(onboard)).getAsJsonObject().get("customerParameters").getAsJsonArray();
-            return parameters;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } 
-        return null;
+        JsonArray parameters = parser.parse(onboard).getAsJsonObject().get("customerParameters").getAsJsonArray();
+        return parameters;
     }
 }
